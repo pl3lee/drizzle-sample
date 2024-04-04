6e8fc3b28e588376363792aab009df31cf9d6064
pl3lee <billy.pl.lee@gmail.com>
2024-04-04 14:56:58 +0000

    done

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,4 +1,8 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import { index, integer, pgEnum, pgTable, uniqueIndex, uuid, varchar, unique, boolean, real, timestamp, primaryKey } from "drizzle-orm/pg-core"
+
+// remember to export this!
+export const UserRole = pgEnum("userRole", ["ADMIN", "BASIC"])
 
 // we are creating a table called "user"
 export const UserTable = pgTable("user", {
@@ -8,5 +12,116 @@ export const UserTable = pgTable("user", {
     // serial("id").primaryKey()
     // serial is a function that generates an auto incrementing id
     id: uuid("id").primaryKey().defaultRandom(),
-    name: varchar("name", { length: 255 }).notNull()
+    name: varchar("name", { length: 255 }).notNull(),
+    age: integer("age").notNull(),
+    // no need to enforce that the email is unique, since we are already using uniqueIndex below
+    email: varchar("email", { length: 255 }).notNull(),
+    // either BASIC or ADMIN, defined as an enum above, with default value BASIC
+    role: UserRole("userRole").default("BASIC").notNull()
+}, table => {
+    return {
+        // we are defining an index on the email column so that it is faster to query
+        // or we can just do index("emailIndex").on(table.email) if we do not need it to be unique
+        emailIndex: uniqueIndex("emailIndex").on(table.email),
+        // the combination of name and age must be unique
+        uniqueNameAndAge: unique("uniqueNameAndAge").on(table.name, table.age)
+    }
+})
+
+export const UserPreferencesTable = pgTable("userPreferences", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    emailUpdates: boolean("emailUpdates").notNull().default(false),
+    // references creates a foreign key to the user table
+    userId: uuid("userId")
+        .references(() => UserTable.id, { onDelete: "cascade", onUpdate: "cascade" })
+        .notNull()
+})
+
+export const PostTable = pgTable("post", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: varchar("title", { length: 255 }).notNull(),
+    // this is a rating between 1-5 that is a decimal number. In postgres, we use the real type
+    averageRating: real("averageRating").notNull().default(0),
+    // timestamps for when the post was created and updated
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+    // the userId that created this post
+    authorId: uuid("authorId").references(() => UserTable.id).notNull()
+})
+
+export const CategoryTable = pgTable("category", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+})
+
+// Note that we do not need an id for this table, since the combination of postId and categoryId is unique and thus can be used as a primary key
+export const PostCategoryTable = pgTable("postCategory", {
+    // the post id
+    postId: uuid("postId").references(() => PostTable.id).notNull(),
+    // the category id
+    categoryId: uuid("categoryId").references(() => CategoryTable.id).notNull()
+}, table => {
+    return {
+        // creating a composite primary key on postId and categoryId
+        pk: primaryKey({ columns: [table.postId, table.categoryId] })
+    }
+})
+
+
+// Relations
+
+// here we are saying our user table has one user preference from the user preferences table, and many posts from the post table.
+// user 1-1 user preferences
+// user 1-many post
+export const UserTableRelations = relations(UserTable, ({ one, many }) => {
+    return {
+        preferences: one(UserPreferencesTable),
+        posts: many(PostTable)
+    }
+})
+
+// for the table that has a foreign key with a 1-1 relationship, we have to pass in another argument to the relations function to specify the foreign key
+// user preferences 1-1 user
+export const UserPreferencesTableRelations = relations(UserPreferencesTable, ({ one }) => {
+    return {
+        user: one(UserTable, {
+            fields: [UserPreferencesTable.userId],
+            references: [UserTable.id]
+        })
+    }
+})
+
+// for a 1-many relationship, whichever table is the 1 side will have to specify the foreign key
+// no need to specify foreign key for the many side
+// post many-1 user
+export const PostTableRelations = relations(PostTable, ({ one, many }) => {
+    return {
+        author: one(UserTable, {
+            fields: [PostTable.authorId],
+            references: [UserTable.id]
+        }),
+        postCategories: many(PostCategoryTable)
+    }
+})
+
+// category 1-many post category
+export const CategoryTableRelations = relations(CategoryTable, ({ many }) => {
+    return {
+        postCategories: many(PostCategoryTable)
+    }
+})
+
+// post category 1-1 post
+// post category many-1 category
+export const PostCategoryTableRelations = relations(PostCategoryTable, ({ one }) => {
+    return {
+        post: one(PostTable, {
+            fields: [PostCategoryTable.postId],
+            references: [PostTable.id]
+        }),
+        category: one(CategoryTable, {
+            fields: [PostCategoryTable.categoryId],
+            references: [CategoryTable.id]
+        })
+    }
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { db } from './drizzle/db';
-import { UserTable } from './drizzle/schema';
+import { UserPreferencesTable, UserTable } from './drizzle/schema';
+import { asc, between, count, eq, gt, sql } from 'drizzle-orm';
 dotenv.config();
 
 export const app = express();
@@ -11,18 +12,126 @@ app.use(express.json());
 
 
 app.put("/user", async (req, res) => {
-    const { name } = req.body
+    const { name, age, email, role } = req.body
+    if (!name || !age || !email) {
+        res.status(400).json({ error: "name, age, and email are required" })
+        return
+    }
     try {
-        await db.insert(UserTable).values({
-            name: name
-        })
-        const user = await db.query.UserTable.findFirst()
-        res.json(user)
+        const user = await db.insert(UserTable).values({
+            name: name,
+            age: age,
+            email: email,
+            ...{ role: role && role }
+        }).returning({
+            // our key id maps to UserTable.id
+            id: UserTable.id
+        }).onConflictDoNothing()
+        res.status(200).json(user)
     } catch (e) {
         res.status(500).json(e)
     }
 })
 
+app.get("/users", async (req, res) => {
+    try {
+        const users = await db.query.UserTable.findMany({
+            // leaving this blank is going to return all columns
+            // you can also set a field to false to exclude it
+            columns: { name: true, email: true },
+            // this allows us to run raw SQL to return extra information
+            extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") },
+            offset: 1,
+            limit: 5,
+            // can leave preferences: true if we want to have all the columns from preferences
+            with: {
+                preferences: {
+                    columns: {
+                        emailUpdates: true
+                    }
+                },
+                posts: {
+                    with: {
+                        postCategories: true
+                    }
+                }
+            },
+            // or desc
+            orderBy: asc(UserTable.name),
+            where: (table, funcs) => funcs.between(table.age, 20, 25)
+        })
+        res.json(users)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
+app.get("/users/sqlstyle", async (req, res) => {
+    try {
+        const users1 = await db
+            .select({ id: UserTable.id, age: UserTable.age, emailUpdates: UserPreferencesTable.emailUpdates })
+            .from(UserTable)
+            .where(between(UserTable.age, 10, 30))
+            .leftJoin(UserPreferencesTable, eq(UserPreferencesTable.userId, UserTable.id))
+            .orderBy(asc(UserTable.name))
+
+        // grouping by name and counting the number of users with that name, and only returning the ones with more than 1
+        const users2 = await db
+            .select({
+                name: UserTable.name,
+                count: count(UserTable.name)
+            })
+            .from(UserTable)
+            .groupBy(UserTable.name)
+            .having(columns => gt(columns.count, 1))
+        res.json({ users1, users2 })
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
+app.post("/user", async (req, res) => {
+    const { id, name, age, email, role } = req.body
+    if (!id) {
+        return res.status(400).json({ error: "id is required" })
+    }
+    try {
+        const updatedUser = await db.update(UserTable).set({
+            ...{ name: name && name },
+            ...{ age: age && age },
+            ...{ email: email && email },
+            ...{ role: role && role }
+        }).where(eq(UserTable.id, id)).returning({
+            id: UserTable.id,
+            name: UserTable.name,
+            age: UserTable.age,
+            email: UserTable.email,
+            role: UserTable.role
+        })
+        res.json(updatedUser)
+    } catch (e) {
+        return res.status(500).json(e)
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    const { id } = req.body
+    if (!id) {
+        return res.status(400).json({ error: "id is required" })
+    }
+    try {
+        const deletedUser = await db.delete(UserTable).where(eq(UserTable.id, id)).returning({
+            id: UserTable.id,
+            name: UserTable.name,
+            age: UserTable.age,
+            email: UserTable.email,
+            role: UserTable.role
+        })
+        res.json(deletedUser)
+    } catch (e) {
+        return res.status(500).json(e)
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
